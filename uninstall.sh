2b827aa4ddb0aeec503761d997e71e992d90ae24
chriswg3 <120425754+chriswg3@users.noreply.github.com>
2024-11-07 11:15:29 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  
 DAEMON_NAME=${SCRIPT_DIR##*/}
 
 rm /service/$DAEMON_NAME
-kill $(pgrep -f "python $SCRIPT_DIR/$DAEMON_NAME")
-kill $(pgrep -f 'supervise $DAEMON_NAME')
+kill $(pgrep -f "supervise $DAEMON_NAME")
+kill $(pgrep -f "python $SCRIPT_DIR/dbus-tempcontrol.py")
 chmod a-x $SCRIPT_DIR/service/run
-$SCRIPT_DIR/restart.sh
+sed -i "s/\/data\/$DAEMON_NAME\/install.sh//" /data/rc.local
